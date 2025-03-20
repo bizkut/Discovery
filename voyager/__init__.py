2b5739ca7966ea7eafe22b51e94eee0b765a881a
Mega-Gorilla <hahahadesu@gmail.com>
2025-03-20 15:23:20 +0000

    Langflowを__all__リストから削除し、VoyagerとVoyager_devboxのみを含めるように変更しました。これにより、モジュールの公開インターフェースが整理されました。

--- voyager/__init__.py
+++ voyager/__init__.py
@@ -1,4 +1,3 @@
 from .voyager import Voyager
 from .devbox import Voyager_devbox
-from .langflow import Langflow
-__all__ = ["Voyager", "Voyager_devbox", "Langflow"]
+__all__ = ["Voyager", "Voyager_devbox"]
